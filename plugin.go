86458702eb02d13d49d9513e1c52addd2179c9a1
idk <idk@idk.local>
2019-03-07 23:30:23 -0500

    load both plugins in this version

--- plugin.go
+++ plugin.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	i2p "github.com/RTradeLtd/go-ipfs-plugin-i2p-gateway/i2p"
-	plugin "github.com/ipfs/go-ipfs/plugin"
+	bootstrapi2p "github.com/RTradeLtd/go-ipfs-plugin-i2p-bootstrap/i2p"
+    plugin "github.com/ipfs/go-ipfs/plugin"
 )
 
 // Plugins is an exported list of plugins that will be loaded by go-ipfs.
 var Plugins = []plugin.Plugin{
 	&i2p.I2PGatePlugin{},
+    &bootstrapi2p.I2PBootstrapPlugin{},
 }
